a2fe200deab8befd2d5bf1565961a6c4b48e383c
hoped <hoped@eofe7.mit.edu>
2022-11-25 21:00:05 -0500

    clear caching testing

--- training_data/queries/15.sql
+++ training_data/queries/15.sql
@@ -2,17 +2,18 @@
 -- TPC-H/TPC-R Top Supplier Query (Q15)
 -- Functional Query Definition
 -- Approved February 1998
-create view revenue (supplier_no, total_revenue) as
+with revenue as (
 	select
-		l_suppkey,
-		sum(l_extendedprice * (1 - l_discount))
+		l_suppkey as supplier_no,
+		sum(l_extendedprice * (1 - l_discount)) as total_revenue
 	from
 		lineitem
 	where
 		l_shipdate >= date '1996-01-01'
 		and l_shipdate < date '1996-01-01' + interval '3' month
 	group by
-		l_suppkey;
+		l_suppkey
+)
 
 select
 	s_suppkey,
@@ -29,9 +30,7 @@ where
 		select
 			max(total_revenue)
 		from
-			revenue:s
+			revenue
 	)
 order by
 	s_suppkey;
-
-drop view revenue;
